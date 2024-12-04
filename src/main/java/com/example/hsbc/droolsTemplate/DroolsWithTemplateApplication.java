19d95d40599af32c17393273ca2237cd1695fddc
Rahulkg0077 <rahul.10820651@ltimindtree.com>
2024-12-04 12:00:06 +0530

    First commit

---
+++ src/main/java/com/example/hsbc/droolsTemplate/DroolsWithTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.hsbc.droolsTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsWithTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsWithTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hsbc/droolsTemplate/DroolsWithTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hsbc.droolsTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsWithTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
